6629cf259359ac6b4bc04c42c33b8156706e5156
Pradyuman <meetupradyuman@gmail.com>
2024-09-07 22:38:06 +0530

    ls command added

--- src/commands/ls.rs
+++ src/commands/ls.rs
@@ -1,8 +1,47 @@
+use colored::Colorize;
+use std::fs;
+use std::path::Path;
+
+//TODO: Extend the functionality in future to get all these values using metadata
+// struct Entity {
+//     permissions: String,
+//     created_at: String,
+//     accessed_at: String,
+//     size: u64,
+//     is_folder: bool,
+//     name: String
+// }
+
 pub fn execute(dirs: &[String]) {
     for dir in dirs {
-        println!("{} contains following files:", dir);
+        if dir == "." {
+            println!("Present working directory contains following content:");
+        } else {
+            println!("{} contains following content:", dir);
+        }
         list_dir(dir);
     }
 }
 
-fn list_dir(_dir: &String) {}
+fn list_dir(dir: &String) {
+    let path = Path::new(dir);
+
+    match fs::read_dir(path) {
+        Ok(entries) => {
+            for entry in entries {
+                match entry {
+                    Ok(entry) => {
+                        let path = entry.path();
+                        if path.is_dir() {
+                            println!("{}", path.display().to_string().green());
+                        } else {
+                            println!("{}", path.display().to_string().blue());
+                        }
+                    }
+                    Err(e) => println!("Error reading entry: {}", e),
+                }
+            }
+        }
+        Err(e) => println!("Error reading directory: {}", e),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,13 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let command = args[2].clone();
+    let command = args[1].clone();
+    println!("{}", command.clone());
     match command.as_str() {
-        "echo" => commands::echo::execute(args[3].clone()),
-        "cat" => commands::cat::execute(&args[3..]),
-        "ls" => commands::ls::execute(&args[3..]),
-        "grep" => commands::grep::execute(args[3].clone(), args[4].clone()),
+        "echo" => commands::echo::execute(args[2].clone()),
+        "cat" => commands::cat::execute(&args[2..]),
+        "ls" => commands::ls::execute(&args[2..]),
+        "grep" => commands::grep::execute(args[2].clone(), args[2].clone()),
         _ => println!("Not supported"),
     }
 }
