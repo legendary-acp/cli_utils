b165a6f5fb04420ab39b761c664de69fb5011b60
Pradyuman <meetupradyuman@gmail.com>
2024-09-07 22:37:55 +0530

    Colour Highlight in Grep command

--- src/commands/grep.rs
+++ src/commands/grep.rs
@@ -1,10 +1,24 @@
+use colored::Colorize;
 use std::fs::read_to_string;
+
+fn print_highlight_word(sentence: &str, word: &str) {
+    if let Some(pos) = sentence.find(word) {
+        let before = &sentence[..pos];
+        let after = &sentence[pos + word.len()..];
+
+        let red_word = word.blue();
+
+        println!("{}{}{}", before, red_word, after);
+    } else {
+        println!("{}", sentence);
+    }
+}
 
 pub fn execute(file_name: String, text: String) {
     for line in read_to_string(file_name).unwrap().lines() {
         let cur_line = line.to_string();
         if cur_line.contains(text.as_str()) {
-            println!("{}", cur_line)
+            print_highlight_word(cur_line.as_str(), text.as_str());
         }
     }
 }
